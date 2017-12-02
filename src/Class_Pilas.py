0b9bfe96474d7255749c91f8993a9614e103f5b7
carloscespedes <cespedescarlos0610@gmail.com>
2017-12-02 09:39:05 -0400

    Creando pila constructor

---
+++ src/Class_Pilas.py
@@ -0,0 +1,12 @@
+
+class Pila:
+    
+    def __init__(self, capacidad):
+        self.capacidad=capacidad
+        self.pila=list()
+        self.puntero=1
+    
+    def crear(self):
+        return("pila creada")
+    
+    
--- src/python_pilas.py
+++ src/python_pilas.py
@@ -1,6 +1,4 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+from Class_Pilas import *
 
-if __name__ == "__main__":
-    print("Hello World")
+pila = Pila(9)
+print(pila.crear())
