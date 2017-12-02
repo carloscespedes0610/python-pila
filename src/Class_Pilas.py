2722428f6b9266b5b16fdc602eff2f3572db54ba
carloscespedes <cespedescarlos0610@gmail.com>
2017-12-02 10:01:48 -0400

    metodo push

--- src/Class_Pilas.py
+++ src/Class_Pilas.py
@@ -17,6 +17,16 @@ class Pila:
             return 'true'
         else:
             return 'false'
+        
+    def Push(self, elemento):
+        if(self.Pila_Llena()=='true'):
+            return "Pila Llena, el elemento "+str(elemento)+" no pudo ser agregado"
+        else:
+            self.pila.append(elemento)
+            self.puntero=self.puntero+1
+            return "elemento agregado"
     
+    def Imprimir_Pila(self):
+        print(str(self.pila)+"; posicion_puntero: "+str(self.puntero)+"; capacidad: "+str(self.capacidad))
     
     
--- src/python_pilas.py
+++ src/python_pilas.py
@@ -1,5 +1,9 @@
 from Class_Pilas import *
 
-pila = Pila(9)
-print(pila.Pila_Vacia())
-print(pila.Pila_Llena())
+pila = Pila(2)
+print("pila vacia: "+pila.Pila_Vacia())
+print("pila llena: "+pila.Pila_Llena())
+print("push: "+pila.Push(8))
+print("push: "+pila.Push(2))
+print("push: "+pila.Push(5))
+pila.Imprimir_Pila()
